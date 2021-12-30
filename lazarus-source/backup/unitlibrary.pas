72044c4a262f7b80f64b09075450c90cf321833e
Jacques <sansmarque@gmail.com>
2021-12-29 21:04:21 -0500

    FormLibrary controls adjustment

--- lazarus-source/backup/unitlibrary.pas
+++ lazarus-source/backup/unitlibrary.pas
@@ -14,7 +14,7 @@
 //
 //     You should have received a copy of the GNU General Public License
 //     along with stm32_eforth.  If not, see <http://www.gnu.org/licenses/>.
-////
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 unit unitLibrary;
